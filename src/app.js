553a7463e48917499d4e6879c13aa9811c66f918
Dan Roberts <dan@adadevelopersacademy.org>
2016-11-23 22:54:05 -0800

    Lesson 2.2: Building a view for an existing element
    Add TaskListView, which manages the HTML for an entire task list
    Move logic for rendering the list out of document.ready
    
    One of the important points here is the split between initialize() and
    render(), and figuring out what ought to go where. Remember that
    initialize() is called once, when the view is first created, while
    render() may be called multiple times. This means that any heavy setup
    work (compiling templates, initializing sub-views, finding interesting
    sub-elements) should be done in initialize(), and render() should be
    kept as skinny as possible.

--- src/app.js
+++ src/app.js
@@ -47,24 +47,51 @@ var TaskView = Backbone.View.extend({
   }
 });
 
+var TaskListView = Backbone.View.extend({
+  initialize: function(options) {
+    // Store a reference to the full list of tasks
+    this.taskData = options.taskData;
+
+    // Compile a template to be shared between the individual tasks
+    this.taskTemplate = _.template($('#task-template').html());
+
+    // Note that we do not need to save el. Because el
+    // is a backbone thing, it copies it for us.
+
+    // Keep track of the <ul> element
+    this.listElement = this.$('.task-list');
+
+    // Create a TaskView for each task
+    this.taskViews = [];
+    this.taskData.forEach(function(task) {
+      var taskView = new TaskView({
+        task: task,
+        template: this.taskTemplate
+      });
+      this.taskViews.push(taskView);
+    }, this);
+  },
+
+  render: function() {
+    // Make sure the list in the DOM is empty
+    // before we start appending items
+    this.listElement.empty();
+
+    // Loop through the data assigned to this view
+    this.taskViews.forEach(function(taskView) {
+      // Cause the task to render
+      taskView.render();
+
+      // Add that HTML to our task list
+      this.listElement.append(taskView.$el);
+    }, this);
+  }
+});
+
 $(document).ready(function() {
-  // Compile a template for our tasks.
-  // The template only needs to be generated once,
-  // but the source lives in the document.
-  var taskTemplate = _.template($('#task-template').html());
-
-  // Loop through our sample data
-  taskData.forEach(function(task) {
-    // Use our template to build some HTML
-    var taskView = new TaskView({
-      task: task,
-      template: taskTemplate
-    });
-
-    // Cause the task to render
-    taskView.render();
-
-    // Add that HTML to our task list
-    $('#task-list').append(taskView.$el);
+  var taskListView = new TaskListView({
+    el: $('#application'),
+    taskData: taskData
   });
+  taskListView.render();
 });
