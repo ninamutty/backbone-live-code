db6ac9a62a8fa36e0351aad6cb921b1714e53a3f
Dan Roberts <dan@adadevelopersacademy.org>
2016-11-23 21:42:10 -0800

    Add hard-coded todo data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import $ from 'jquery';
 
+var taskData = [
+  {
+    title: 'Mow the lawn',
+    description: 'Must be finished before BBQ on Sat afternoon',
+    assignedTo: ['Kari', 'Charles']
+  }, {
+    title: 'Go to the Bank',
+    description: 'Need to make a transfer',
+    assignedTo: ['Dan', 'Jamie', 'Chris']
+  }, {
+    title: 'Tune the Piano',
+    description: 'High C is missing or something???',
+    assignedTo: ['Crystal']
+  }
+];
+
 $(document).ready(function() {
   $('#test-area').append($('<p>Hello World!</p>'));
 });
