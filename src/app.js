e0467119356677dc3b65aa7d8967b464fe69cd5e
Dan Roberts <dan@adadevelopersacademy.org>
2016-11-23 22:21:12 -0800

    Lesson 2.1: Simple views
    Add a TaskView, which manages the HTML used to display a task
    Use TaskViews to render tasks, rather than raw templates.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import _ from 'underscore';
+import Backbone from 'backbone';
 
 var taskData = [
   {
@@ -14,6 +15,38 @@ var taskData = [
   }
 ];
 
+var TaskView = Backbone.View.extend({
+  // initialize() will be called automatically
+  // when the view is first created.
+  initialize: function(options) {
+    // Because the template can be reused, it makes sense
+    // to share one between all our views.
+    this.template = options.template;
+
+    // For now this view is wrapped around a regular old
+    // JavaScript object.
+    this.task = options.task;
+  },
+
+  // render() should be in charge of changing the
+  // HTML managed by this view.
+  // For now we'll be calling it manually, but later
+  // we'll set up Backbone to call it for us.
+  //
+  // Note that when the element managed by the view
+  // has been attached to the DOM, if we change the
+  // view's data the page will update as soon as we
+  // call render() again.
+  render: function() {
+    // Use the task template to build some HTML, and
+    // add it to our DOM object
+    this.$el.html(this.template({task: this.task}));
+
+    // Enable chained calls
+    return this;
+  }
+});
+
 $(document).ready(function() {
   // Compile a template for our tasks.
   // The template only needs to be generated once,
@@ -23,9 +56,15 @@ $(document).ready(function() {
   // Loop through our sample data
   taskData.forEach(function(task) {
     // Use our template to build some HTML
-    var entry = taskTemplate({task: task});
+    var taskView = new TaskView({
+      task: task,
+      template: taskTemplate
+    });
+
+    // Cause the task to render
+    taskView.render();
 
     // Add that HTML to our task list
-    $('#task-list').append($(entry));
+    $('#task-list').append(taskView.$el);
   });
 });
