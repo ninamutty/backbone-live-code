367edff8f7bb205437a087271fc210e3b1395600
Dan Roberts <dan@adadevelopersacademy.org>
2016-11-23 23:30:47 -0800

    Remove assignedTo field from hard-coded data
    It will make our HTML much more complicated later, and since we're not
    focusing on Backbone until next week we don't need it now.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,13 @@ import $ from 'jquery';
 var taskData = [
   {
     title: 'Mow the lawn',
-    description: 'Must be finished before BBQ on Sat afternoon',
-    assignedTo: ['Kari', 'Charles']
+    description: 'Must be finished before BBQ on Sat afternoon'
   }, {
     title: 'Go to the Bank',
-    description: 'Need to make a transfer',
-    assignedTo: ['Dan', 'Jamie', 'Chris']
+    description: 'Need to make a transfer'
   }, {
     title: 'Tune the Piano',
-    description: 'High C is missing or something???',
-    assignedTo: ['Crystal']
+    description: 'High C is missing or something???'
   }
 ];
 
