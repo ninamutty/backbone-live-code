a95933b7daecc975f4fdffdd961ba7b8044f3efc
Nina Mutty <ninamutty@gmail.com>
2016-11-28 13:40:30 -0800

    application views

--- src/app.js
+++ src/app.js
@@ -45,26 +45,81 @@ var TaskView = Backbone.View.extend({
 
 
 
-// view instances are created by using the keyword 'new'
-// var card = new TaskView();
+var TaskListView = Backbone.View.extend({
+  initialize: function(options) {
+    // Store a the full list of tasks
+    this.taskData = options.taskData;
 
+    // Compile a template to be shared between the individual tasks
+    this.taskTemplate = _.template($('#task-template').html());
+
+    // Keep track of the <ul> element
+    this.listElement = this.$('.task-list');
+
+    // Create a TaskView for each task
+    this.cardList = [];
+    this.taskData.forEach(function(task) {
+      var card = new TaskView({
+        task: task,
+        template: this.taskTemplate
+      });
+      this.cardList.push(card);
+    }, this);
+  },
+
+
+  render: function() {
+    // Make sure the list in the DOM is empty
+    // before we start appending items
+    this.listElement.empty(); 
+
+    // Loop through the data assigned to this view
+    this.cardList.forEach(function(card) {
+      // Cause the task to render
+      card.render();  
+
+      // Add that HTML to our task list
+      this.listElement.append(card.$el);
+    }, this);
+
+    return this; // enable chained calls
+  }
+});
 
 
 $(document).ready(function() {
-  var taskTemplate = _.template($('#task-template').html()); //creating a task template and passing it the jquery object
-  // var generatedHTML = taskTemplate({task: taskData[0]}); /// One task
-
-  var taskListElement = $('.task-list');
-  var cardList = [];
-  taskData.forEach(function(task) {
-    var card = new TaskView({
-      task: task,
-      template: taskTemplate
-    });
-    cardList.push(card);
-    taskListElement.append(card.render().$el);
+  var application = new TaskListView({
+    el: $('#application'),
+    taskData: taskData
   });
+  application.render();
 });
+
+
+
+
+
+
+
+
+
+////////// Pre-Application-View ////////////
+
+// $(document).ready(function() {
+//   var taskTemplate = _.template($('#task-template').html()); //creating a task template and passing it the jquery object
+//   // var generatedHTML = taskTemplate({task: taskData[0]}); /// One task
+//
+//   var taskListElement = $('.task-list');
+//   var cardList = [];
+//   taskData.forEach(function(task) {
+//     var card = new TaskView({
+//       task: task,
+//       template: taskTemplate
+//     });
+//     cardList.push(card);
+//     taskListElement.append(card.render().$el);
+//   });
+// });
 
 
 
